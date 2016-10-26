61e629a6babd8a9d1d058c188c41ed7b4cc97775
Kelley <kelley@kelleygibson.ca>
2016-10-26 10:28:46 -0400

    Upload project files

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# vanilla-filters
-Simple, pure JavaScript content filtering.
+# Pure JavaScript Content Filters
+
+[View the demo.](http://thephuse.github.io/pure-js-content-filters)
+
+### Simple, pure JavaScript content filtering.
+
+#### License: [MIT](https://github.com/thephuse/pure-js-content-filters/license.txt)
+
+## Usage
+
+#### 1. Include the script in the footer of your project.
+```<script type="text/javascript" src="/vanilla-filters/vanilla-filters.js"></script>```
+
+#### 2. Include the stylesheet in the header.
+```<link rel="stylesheet" href="vanilla-filters/style-philters.css" />```
+
+#### 3. Create the HTML for your page, styling as desired. (TO DO)
+Your filter links should have a parent container with a unique ID and/or class. Each link should have a unique hash link (can be descriptive of your filter or anything you'd like). 
+
+The Surprise Me and Clear Filters links are optional, but if included must have a unique class or ID.
+
+For example:
+```html
+  <ul id="filter-links">
+    <li><a href="#philter-1">Filter 1</a></li>
+    <li><a href="#philter-2">Filter 2</a></li>
+    <li><a href="#philter-3">Filter 3</a></li>
+    <li><a href="#philter-4">Filter 4</a></li>
+    <li><a id="surprise-me" href="#surprise-me">Surprise Me</a></li>
+    <li><a id="clear-philters" href="#clear-philters">Clear Filters</a></li>
+  </ul>
+```
+
+Your results (the content being filtered) should also have a parent container with a unique ID or class. Each single result (the immediate child elements of the parent container) should have classes equal to their relevant filter(s) hash links minus the `#`.
+
+For example:
+```html
+  <div id="results">
+    <div class="philter-1">Post 1</div>
+    <div class="philter-1 philter-4">Post 2</div>
+    <div class="philter-2">Post 3</div>
+    <div class="philter-3">Post 4</div>
+    <div class="philter-1 philter-2">Post 5</div>
+    <div class="philter-4">Post 6</div>
+    <div class="philter-1 philter-3 philter-4">Post 7</div>
+    <div class="philter-1 philter-4">Post 8</div>
+    <div class="philter-1 philter-2 philter-4">Post 9</div>
+  </div>  
+```
+
+#### 4. Call the function with your parameters and any desired user options.
+`philterContent(philtersContainer, resultsContainer, userOptions);`
+
+The `philtersContainer` and `resultsContainer` parameters are required. You should replace each with the selector for the containing element of each section. For example: 
+
+`philterContent('#filters-container', '.results-container')`. 
+
+Both parameters can accept classes or IDs.
+
+The `userOptions` object is composed of optional key/value pairs to give you additional control over your filters. Defaults are overridden by providing options for this parameter.
+
+#### Defaults:
+
+````js
+{
+  viewFilters: 'View Filters',
+  hideFilters: 'Hide Filters',
+  clearTarget: '', // Clear Filters element id or class if functionality is desired. Leave as empty string to exclude from your filters.
+  clearFilters: 'Clear Filters',
+  surpriseTarget: '', // Surprise element id or class if functionality is desired. Leave as empty string to exclude from your filters.
+  surpriseMe: 'Surprise Me',
+  warningMsg: 'Whoops! Looks like your search is too narrow. Try removing a selected filter or clear all filters to try again.'
+}
+````
+
+## Caveat
+These filters are designed to work in instances where all filtered content is on the same page. Therefore, the filters will not work properly across paginated results / content.
+
+## Compatibility
+
+This script has been tested in IE 11 and the latest releases of Chrome, Firefox, Safari and mobile Safari browsers.
---
+++ vanilla-filters.js
@@ -0,0 +1,198 @@
+var activeFilters = null;
+
+function philterContent(philtersContainer, resultsContainer, userOptions){
+  "use strict";
+
+  /********************
+   Set Options
+  *********************/
+  let optionDefaults = {
+    viewFilters: 'View Filters',
+    hideFilters: 'Hide Filters',
+    clearTarget: '', // Clear Filters element id or class if desired. Leave as empty string to exclude from your filters.
+    clearFilters: 'Clear Filters',
+    surpriseTarget: '', // Surprise element id or class if desired. Leave as empty string to exclude from your filters.
+    surpriseMe: 'Surprise Me',
+    warningMsg: 'Whoops! Looks like your search is too narrow. Try removing a selected filter or clear all filters to try again.'
+  };
+
+  // combine default and user set options
+  let options = extend(optionDefaults, userOptions);
+
+  /***************************************************
+    Insert Show/Hide Filters content into container
+  ****************************************************/
+  const elContainer = document.querySelector(philtersContainer);
+  elContainer.classList.add('philters-parent');
+  const el = document.createElement('h4');
+  el.className = 'philters-trigger';
+  el.innerHTML = '<span class="default-title">' + options.viewFilters + '</span><span class="alt-title">' + options.hideFilters + '</span>';
+
+  elContainer.parentNode.insertBefore(el, elContainer);
+
+  // Hide filters intially
+  elContainer.classList.add('hide');
+
+  /***************************************************
+    Show/Hide Filters functionality
+  ****************************************************/
+  document.querySelector('.philters-trigger').addEventListener('click', function(e){
+    e.preventDefault();
+    if(elContainer.classList.contains('hide')) {
+      elContainer.classList.remove('hide');
+      this.classList.add('philters-open');
+    } else {
+      elContainer.classList.add('hide');
+      this.classList.remove('philters-open');
+    }
+  });
+
+
+  /*************************
+    Filter Functionality
+  *************************/
+
+  // set the initial state of all results to active (display all results by default)
+  const resultsParent = document.querySelector(resultsContainer);
+  const results = resultsParent.children;
+
+  for (let result of results) {
+    result.classList.add('philter-active');
+  }
+
+  // set filter as active or inactive on click and update results with toggleVisible() function
+  let philterLinks;
+
+  if(options.surpriseTarget !== ''){
+    // if user is including Surprise Me functionality, don't add the filter click function to it
+    philterLinks = document.querySelectorAll(philtersContainer + ' a:not(' + options.surpriseTarget + ')');
+  } else {
+    philterLinks = document.querySelectorAll(philtersContainer + ' a');
+  }
+
+  for (let link of philterLinks) {
+    link.addEventListener('click', function(e){
+      e.preventDefault();
+      if (this.classList.contains('activate-philter')){
+        this.classList.remove('activate-philter');
+      } else {
+        this.classList.add('activate-philter');
+      }
+      toggleVisible();
+    });
+  }
+
+  // Clear Filters functionality. Show if option is true.
+  if(options.clearTarget !== ''){
+    document.querySelector(options.clearTarget).innerHTML = options.clearFilters;
+
+    document.querySelector(options.clearTarget).addEventListener('click', function(e){
+      e.preventDefault();
+      // clear all activated filters and reset results upon click of Clear Filters link
+      for (let reset of document.querySelectorAll('.activate-philter')) {
+        reset.classList.remove('activate-philter');
+      }
+      toggleVisible();
+    });
+  }
+
+  // "Surprise me" functionality -- add in if user has set surpriseTarget
+  if(options.surpriseTarget !== ''){
+    document.querySelector(options.surpriseTarget).innerHTML = options.surpriseMe;
+
+    document.querySelector(options.surpriseTarget).addEventListener('click', function(e){
+      e.preventDefault();
+
+      const activeResults = document.querySelectorAll('.philter-active');
+      const randomNum = Math.floor(Math.random() * activeResults.length);
+
+      // get a random post of the current active results and trigger the click function on it
+      activeResults[randomNum].classList.add('random');
+
+      // hide all results except for randomly selected only
+      for (let hideEm of document.querySelectorAll('.philter-active')) {
+        hideEm.classList.remove('philter-active');
+      }
+
+      // redirect to URL of randomly selected item
+      window.location = document.querySelector('.random a').getAttribute('href');
+    });
+  }
+
+  /*************************
+    Append warning message
+  *************************/
+  const warningMsg = document.createElement('p');
+  warningMsg.id = 'philter-warning';
+  warningMsg.innerHTML = options.warningMsg;
+
+  resultsParent.parentNode.insertBefore(warningMsg, resultsParent.nextSibling);
+
+
+  function toggleVisible() {
+    // create an empty array that we'll add any active filters to
+    activeFilters = [];
+
+    // add href target of activated filters to the activeFilters array
+    const getActiveFilters = document.querySelectorAll(philtersContainer + ' a.activate-philter');
+
+    for (let i = 0; i < getActiveFilters.length; i++) {
+      let hrefTarget = getActiveFilters[i].getAttribute('href').substr(1);
+      activeFilters.push(hrefTarget);
+    }
+
+    // loop through results to show only those with a class of the activated filter(s)
+    for (let single of results) {
+      let showThis = true;
+
+      for (let filter of activeFilters) {
+        if (single.classList.contains(filter)) {
+          showThis;
+        } else {
+          showThis = false;
+        }
+      }
+
+      if(showThis) {
+        single.classList.add('philter-active');
+      } else {
+        single.classList.remove('philter-active');
+      }
+    }
+
+    // if there are no results with the activated filter(s), show the warning / error message
+    const warningAlert = document.querySelector('#philter-warning');
+
+    if(document.querySelectorAll('.result.philter-active').length === 0){
+      warningAlert.classList.add('msg-visible');
+    } else {
+      warningAlert.classList.remove('msg-visible');
+    }
+  }
+}
+
+/*************************************************
+  Combine default and any user set parameters
+*************************************************/
+function extend () {
+  // Variables
+  let extended = {};
+
+  // Merge the object into the extended object
+  let merge = function (obj) {
+    for ( let prop in obj ) {
+      if ( obj.hasOwnProperty( prop ) ) {
+        extended[prop] = obj[prop];
+      }
+    }
+  };
+
+  // Loop through each object and conduct a merge
+  for (let i = 0; i < arguments.length; i++ ) {
+    var obj = arguments[i];
+    merge(obj);
+  }
+
+  return extended;
+};
+
